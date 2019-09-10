8daa268f831ecb16e150ce221b18e8a8fb050177
Haris Spahija <haris.spahija@hybrit.org>
2019-09-10 21:31:01 +0200

    init

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const graphqlHTPP = require("express-graphql");
+const schema = require("./schema/schema");
+
+const app = express();
+
+app.use(
+  "/graphql",
+  graphqlHTPP({
+    schema,
+    graphiql: true
+  })
+);
+
+app.listen(3000, () => {
+  console.log("App listening on port 3000!");
+});
