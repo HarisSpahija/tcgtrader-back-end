47e09122b04b3ffedd94b638519d8ddeb43a92d4
Haris Spahija <haris.spahija@hybrit.org>
2019-09-11 00:08:14 +0200

    added queries and types

---
+++ schema/types.js
@@ -0,0 +1,29 @@
+const graphql = require("graphql");
+const { GraphQLObjectType, GraphQLString } = graphql;
+
+const UserType = new GraphQLObjectType({
+    name: "User",
+    type: "Query",
+    fields: {
+      id: { type: GraphQLString },
+      username: { type: GraphQLString },
+      email: { type: GraphQLString },
+      joined: { type: GraphQLString },
+      last_logged_in: { type: GraphQLString }
+    }
+  });
+  
+  const ProjectType = new GraphQLObjectType({
+    name: "Project",
+    type: "Query",
+    fields: {
+      id: { type: GraphQLString },
+      creator_id: { type: GraphQLString },
+      created: { type: GraphQLString },
+      title: { type: GraphQLString },
+      description: { type: GraphQLString }
+    }
+  });
+  
+  exports.UserType = UserType;
+  exports.ProjectType = ProjectType;
