be0af7191eb4ebe4833fb057e0278fec1dad0b1a
Haris Spahija <haris.spahija@hybrit.org>
2019-09-14 14:59:38 +0200

    added card search on name and card text

--- src/index.js
+++ src/index.js
@@ -28,6 +28,40 @@ const users = [
     email: "sophie@example.com"
   }
 ];
+
+const cards = [
+  {
+    id: "1",
+    name: "Ranging Raptors",
+    type: "Creature - Dinosaur",
+    cardText:
+      "Enrage - Whenever Ranging Raptors is dealt damage, you may search your library for a basic land card, put i tonto the battlefield tapped, then shuffle your library.",
+    flavourText: "They cover their territory like a tide of teeth and claws.",
+    convertedManaCost: 3,
+    standardLegal: true,
+    price: 0.35
+  },
+  {
+    id: "2",
+    name: "Lesser Masticore",
+    type: "Artifact Creature - Masticore",
+    cardText:
+      "As an additional cost to cast this spell, discard a card. {4}: Lesser Masticore deals 1 damage to target creature. Persist",
+    convertedManaCost: 2,
+    standardLegal: false,
+    price: 0.15
+  },
+  {
+    id: "3",
+    name: "Terramorphic Expanse",
+    type: "Land",
+    cardText:
+      "{T}, Sacrifice Terramorphic Expanse: Search your library for a basic land card and put it onto the battlefield tapped. Then shuffle your library.",
+    flavourText: "Take two steps north into the unsettled future, south into the unquiet past, easy into the present day, or west into the great unknown.",
+    standardLegal: false,
+    price: 0.50
+  }
+];
 // Type definitions (schema)
 // All possible types:
 // (scalar) - String, Bolean, Int, Float, ID
@@ -35,17 +69,18 @@ const typeDefs = `
     type Query {
         users(query: String): [User!]!
         me: User!
-        card: Card!
+        cards(query: String): [Card!]!
     }
 
     type Card {
         id: ID!
         name: String!
+        type: String!
         cardText: String!
         flavourText: String
-        convertedManaCost: Int!
+        convertedManaCost: Int
         standardLegal: Boolean!
-        price: Float  
+        price: Float!  
     }
 
     type User {
@@ -66,58 +101,26 @@ const resolvers = {
   Query: {
     users(parent, args, ctx, info) {
       if (!args.query) {
-        return users
+        return users;
       }
 
-      return users.filter((user) => {
-        return user.name.toLowerCase().includes(args.query.toLowerCase())
-      })
+      return users.filter(user => {
+        return user.name.toLowerCase().includes(args.query.toLowerCase());
+      });
     },
-    me() {
-      return {
-        id: "1231241",
-        name: "Haris Spahija",
-        email: "haris.spahija@hybrit.org",
-        age: 22
-      };
+    cards(parent, args, ctx, info) {
+      if (!args.query) {
+        return cards;
+      }
+
+      return cards.filter(card => {
+        const isNamematch = card.name.toLowerCase().includes(args.query.toLowerCase());
+        const isTypeMatch = card.type.toLowerCase().includes(args.query.toLowerCase());
+        return isNamematch || isTypeMatch
+      });
     },
-    card() {
-      return {
-        id: "1",
-        name: "Ranging Raptors",
-        cardText:
-          "Enrage - Whenever Ranging Raptors is dealt damage, you may search your library for a basic land card, put i tonto the battlefield tapped, then shuffle your library.",
-        flavourText:
-          "They cover their territory like a tide of teeth and claws.",
-        convertedManaCost: 3,
-        standardLegal: true,
-        price: 0.35
-      };
-    }
   },
-  Card: {
-    id() {
-      return 1;
-    },
-    name() {
-      return "Ranging Raptors";
-    },
-    convertedManaCost() {
-      return 3;
-    },
-    standardLegal() {
-      return true;
-    },
-    price() {
-      return 0.32;
-    },
-    cardText() {
-      return "Enrage - Whenever Ranging Raptors is dealt damage, you may search your library for a basic land card, put i tonto the battlefield tapped, then shuffle your library.";
-    },
-    flavourText() {
-      return "They cover their territory like a tide of teeth and claws.";
-    }
-  },
+  
   User: {
     collection() {
       return {
