03a7765b47c52d40ddbb6d7d42e1aa8c2a8b7669
Haris Spahija <haris.spahija@hybrit.org>
2019-09-23 21:03:19 +0200

    chore moved some comments to a readme.md

---
+++ README.md
@@ -0,0 +1,5 @@
+// Resolvers
+// parent = contains arguments supplied from parent resolver
+// arg = contains arguments supplied from query
+// ctx = context for contextual data such as login tokens
+// info = information about the operation sent to server
--- src/index.js
+++ src/index.js
@@ -7,12 +7,6 @@ import Query from './resolvers/Query';
 import User from './resolvers/User';
 import WantList from './resolvers/WantList';
 
-// Resolvers
-// parent = contains arguments supplied from parent resolver
-// arg = contains arguments supplied from query
-// ctx = context for contextual data such as login tokens
-// info = information about the operation sent to server
-
 const server = new GraphQLServer({
   typeDefs: "./src/schema.graphql",
   resolvers: {
